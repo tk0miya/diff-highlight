af40087e14cf9375857d5e124eaa7015e6a43365
tk0miya <i.tkomiya@gmail.com>
2013-12-18 18:42:01 +0900

    Change order of copyrights

--- diff-highlight.py
+++ diff-highlight.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-#  Copyright (c) 2001-2013 Python Software Foundation; All Rights Reserved
 #  Copyright 2013 Takeshi KOMIYA
+#  Copyright (c) 2001-2013 Python Software Foundation; All Rights Reserved
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
