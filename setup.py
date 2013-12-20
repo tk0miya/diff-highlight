ab30ae49a76365d6e50a3b7fd689f94c0f47f438
tk0miya <i.tkomiya@gmail.com>
2013-12-20 20:41:46 +0900

    Package scripts for distribution

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 4 - Production/Beta",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Version Control",
+    "Topic :: Text Processing :: Filters",
+]
+
+setup(
+    name='diff-highlight',
+    version='0.1.0',
+    description='pretty diff highlighter; emphasis changed words in diff',
+    long_description='',
+    classifiers=classifiers,
+    keywords=['mercurial', 'git', 'diff', 'highlight'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://blockdiag.com/',
+    download_url='http://pypi.python.org/pypi/diff-highlight',
+    license='Apache License 2.0',
+    py_modules=['diff_highlight'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+)
