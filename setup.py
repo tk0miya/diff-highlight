440a833cfa36ea24eca735310b77607e3b11a6b2
tk0miya <i.tkomiya@gmail.com>
2013-12-21 17:03:28 +0900

    Drop py3 environments

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ classifiers = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.3",
     "Topic :: Software Development",
     "Topic :: Software Development :: Version Control",
     "Topic :: Text Processing :: Filters",
