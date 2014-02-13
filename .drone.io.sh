086232c5c03360bea7c746704fe603f8107b1acc
tk0miya <i.tkomiya@gmail.com>
2014-02-13 15:36:44 +0900

    Add .drone.io.sh

---
+++ .drone.io.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python2.4 python2.4-dev python2.5 python2.5-dev python2.6 python2.6-dev
+
+virtualenv old_pythons
+old_pythons/bin/pip install pip==1.3 virtualenv==1.7.2 tox==1.4
+old_pythons/bin/tox -e py24,py25
+
+pip install --use-mirrors --upgrade detox misspellings
+find src/ -name "*.py" | misspellings -f -
+detox
