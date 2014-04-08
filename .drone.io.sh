6d1408737b7407bc09b53464b1ed5433d4584a98
tk0miya <i.tkomiya@gmail.com>
2014-04-08 16:49:30 +0900

    Run tests in py34

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install python2.4 python2.4-dev python2.5 python2.5-dev python2.6 python2.6-dev
+sudo apt-get install python2.4 python2.4-dev python2.5 python2.5-dev python2.6 python2.6-dev python3.4 python3.4-dev
 
 virtualenv old_pythons
 old_pythons/bin/pip install pip==1.3 virtualenv==1.7.2 tox==1.4
