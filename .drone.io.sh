a7f2d1952360a9ce004f9b11e03da5cd2cfda34b
tk0miya <i.tkomiya@gmail.com>
2014-06-08 22:53:39 +0900

    Install docutils to old_python env at drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,11 +3,10 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install python2.4 python2.4-dev python2.5 python2.5-dev python2.6 python2.6-dev python3.4 python3.4-dev
 
-pip install --use-mirrors --upgrade detox misspellings docutils
-
 virtualenv old_pythons
-old_pythons/bin/pip install pip==1.3 virtualenv==1.7.2 tox==1.4
+old_pythons/bin/pip install pip==1.3 virtualenv==1.7.2 tox==1.4 docutils
 old_pythons/bin/tox -e py24,py25
 
+pip install --use-mirrors --upgrade detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
