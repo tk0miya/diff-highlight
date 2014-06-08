8a3738d575dd1216e87469cd3e0444a2f9c66125
tk0miya <i.tkomiya@gmail.com>
2014-06-08 22:40:22 +0900

    Install docutils before testing at drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -7,6 +7,6 @@ virtualenv old_pythons
 old_pythons/bin/pip install pip==1.3 virtualenv==1.7.2 tox==1.4
 old_pythons/bin/tox -e py24,py25
 
-pip install --use-mirrors --upgrade detox misspellings
+pip install --use-mirrors --upgrade detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
