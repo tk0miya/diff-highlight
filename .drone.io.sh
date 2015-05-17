b2a9719d10b1dac6ba68f4a60334d7a0d071722a
tk0miya <i.tkomiya@gmail.com>
2015-05-17 14:00:59 +0900

    Use detox from package instead of HEAD

--- .drone.io.sh
+++ .drone.io.sh
@@ -4,6 +4,5 @@ sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
 pip install --use-mirrors --upgrade detox misspellings docutils
-pip install https://bitbucket.org/hpk42/detox/get/tip.zip
 find src/ -name "*.py" | misspellings -f -
 detox
